db4a5a4ccd37504e60bb2fa34dc5d0919f475e49
Le Quoc Bao <baolqse181942@fpt.edu.vn>
2024-09-26 16:03:50 +0700

    login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import React from "react";
-import LoginForm from "./components/loginForm/LoginForm";
-
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Login from "./login/login.jsx";
+import Register from "./register/register";
+import Profile from "./profile/profile.jsx";
 function App() {
-  return (
-    <div>
-      <LoginForm />
-      <LoginForm />
-    </div>
-  );
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+    {
+      path: "/profile",
+      element: <Profile />,
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/config/firebase.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { GoogleAuthProvider, getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDXfxXOmpxmCncmGjZN25PDHLbX8tGXFEE",
+  authDomain: "koi-auction-b576c.firebaseapp.com",
+  projectId: "koi-auction-b576c",
+  storageBucket: "koi-auction-b576c.appspot.com",
+  messagingSenderId: "388140353841",
+  appId: "1:388140353841:web:4a61d26c45f8c48e208810",
+  measurementId: "G-W10X4V6F2S"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const googleprovider = new GoogleAuthProvider();
+
+const auth = getAuth();
+export {googleprovider, auth};
